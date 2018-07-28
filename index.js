16e7a767dc8b92a9609a648af9cde8b8387b4a03
phil <philgoodmusic@gmail.com>
2018-07-27 23:59:09 -0400

    additional refactor

--- index.js
+++ index.js
@@ -2,86 +2,83 @@ const Words = require("./word.js");
 const inquirer = require('inquirer');
 const prompt = require('prompt');
 const wordChoices = [
-    "javascript", "angular", "react",
-    "node", "java", "html",
-    "css", "express", "vue",
-    "swift", "sql"
-  ];
-  
-  
-  let numOfGuesses;
-  let pastWords;
-  let word;
-  let currentWord;
+  "javascript", "angular", "react",
+  "node", "java", "html",
+  "css", "express", "vue",
+  "swift", "sql"
+];
 
-  prompt.start();
- 
-  //
-  // Get two properties from the user: username and email
-  //
-  prompt.get(['username', 'email'], function (err, result) {
-    console.log('Hello, ' + result.username + '! welcome to Programming word guess!');
- 
-    initiate()
-  });
+let numOfGuesses;
+let pastWords;
+let word;
+let currentWord;
 
+prompt.start();
+
+//
+// Get two properties from the user: username and email
+//
+prompt.get(['username', 'email'], function (err, result) {
+  console.log('Hello, ' + result.username + '! welcome to Programming word guess!');
+  initiate()
+});
+
+// initiate the game
 function initiate() {
-    pastWords = [];
+  pastWords = [];
   console.log("------------------------------------------");
   playGame();
 }
 
+// sets start conditions and create new Word
 function playGame() {
-    currentWord = "";
-    numOfGuesses = 15;
-    if(pastWords.length < wordChoices.length) {
-      currentWord = getWord();
-    } else {
-      // WIN CONDITION
-      console.log("You sure do know your programming languages!");
-      continuePrompt();
-    }
-  
-      word = new Words(currentWord);
-      // console.log(word)
-      word.makeLetters();
-      makeGuess();
-    
+  currentWord = "";
+  numOfGuesses = 15;
+  if (pastWords.length < wordChoices.length) {
+    currentWord = getWord();
+  } else {
+    // WIN CONDITION
+    console.log("You sure do know your programming languages!");
+    continuePrompt();
   }
 
-  function getWord() {
-    let random = Math.floor(Math.random() * wordChoices.length);
-    let randomWord = wordChoices[random];
-    if(pastWords.indexOf(randomWord) === -1) {
-      pastWords.push(randomWord);
-      return randomWord;
-    } else {
-      return getWord();
-    }
-  }
+  word = new Words(currentWord);
+  word.makeLetters();
+  makeGuess();
 
-  function makeGuess() {
-    let checker = [];
-    inquirer.prompt([
-      {
-        name: "guessedLetter",
-        message: "\nGuess a letter!" +
-                "\nGuesses Left: " + numOfGuesses
-      }
-    ])
+}
+
+// gets randomm word and pushes chosen word to pastWords array
+function getWord() {
+  let random = Math.floor(Math.random() * wordChoices.length);
+  let randomWord = wordChoices[random];
+  if (pastWords.indexOf(randomWord) === -1) {
+    pastWords.push(randomWord);
+    return randomWord;
+  } else {
+    return getWord();
+  }
+}
+
+// handles user guesses
+function makeGuess() {
+  let checker = [];
+  inquirer.prompt([{
+      name: "guessedLetter",
+      message: "\nGuess a letter!" +
+        "\nGuesses Left: " + numOfGuesses
+    }])
     .then(data => {
       word.letters.forEach(letter => {
         letter.checkLetter(data.guessedLetter);
         checker.push(letter.getCharacter());
-        xxx = letter.isGuessed
-        
       });
-      
+
       console.log(checker.toString())
-      
-      if(numOfGuesses > 0 && checker.indexOf("_") !== -1) {
+
+      if (numOfGuesses > 0 && checker.indexOf("_") !== -1) {
         numOfGuesses--;
-        if(numOfGuesses === 0) {
+        if (numOfGuesses === 0) {
           console.log("SORRY!  GAME OVER!");
           continuePrompt();
         } else {
@@ -93,22 +90,19 @@ function playGame() {
         playGame();
       }
     });
-    
-  }
-  
-  function continuePrompt() {
-    let checker = [];
-    inquirer.prompt([
-      {
-        name: "continue",
-        message: "Do you want to play again?",
-        type: 'confirm'
-      }
-    ])
+}
+
+// asks user to continue
+function continuePrompt() {
+  let checker = [];
+  inquirer.prompt([{
+      name: "continue",
+      message: "Do you want to play again?",
+      type: 'confirm'
+    }])
     .then(data => {
-     if (data.continue) {
-      playGame()
-     } else console.log("Thanks for playing! See you next time!")
-      });
-    }
- 
+      if (data.continue) {
+        playGame()
+      } else console.log("Thanks for playing! See you next time!")
+    });
+}
